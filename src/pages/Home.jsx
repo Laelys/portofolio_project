6deb99d6c0262ed70fa5c12c07634e8b657976da
Laelys <laelyseop@gmail.com>
2024-09-28 17:03:25 +0300

    added first carousel container
    Hero side of the website

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,19 @@
 import Navbar from "../components/Navbar";
 import Carousel from "../components/Carousel";
+import "../styling/home.css";
 
 const Home = () => {
   return (
     <>
       <Navbar />
-      <Carousel />
+      <div className="carousel__container">
+        <div className="container__text">
+          <h2>Your best value proposition.</h2>
+          <p>Insert your paragraph here.</p>
+          <button>Click me!</button>
+        </div>
+        <Carousel />
+      </div>
     </>
   );
 };
